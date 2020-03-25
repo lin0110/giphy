ea7410ce3bf2f3a7950fa91a996a349caea70a22
lin0110 <jiajunlin2016@gmail.com>
2020-03-25 22:08:56 +0000

    One extension

--- js/script.js
+++ js/script.js
@@ -5,17 +5,19 @@
 
 $("#search-button").click(function(){
     let userInput = $("#search-term").val();
-
-    // https://api.giphy.com/v1/gifs/search?q=puppy&api_key=dc6zaTOxFJmzC -> put this in your browser in a new tab
+    let random = Math.round(Math.random() * 24);
+    
+    console.log(random);
+    
     fetch("https://api.giphy.com/v1/gifs/search?q="+ userInput +"&api_key=dc6zaTOxFJmzC") 
       .then(function(response) {
         return response.json();
         })
       .then(function(response) {
         console.log(response);
-        var pic_url = response.data[0].images.downsized_medium.url;
+        var pic_url = response.data[random].images.downsized_medium.url;
         console.log(pic_url);
-        $('#pic').append('<img src=' + pic_url + '>');
+        $('#pic').html('<img src=' + pic_url + '>');
         })
 
     
